392f2ce90eb9bb4031b61beb06ff15db54e39f34
camspringer <81245844+camspringer@users.noreply.github.com>
2021-08-27 17:51:51 -0400

    Add files via upload

---
+++ CAMERON_SPRINGER.php
@@ -0,0 +1,176 @@
+<html lang="en">
+	<!-- https://www.bootdey.com/snippets/view/team-user-resume#html -->
+		<meta charset="utf-8">
+		<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous"><link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.12.1/css/all.min.css" integrity="sha256-mmgLkCYLUQbXn0B1SRqzHar6dCnv9oZFPEC1g1cwlkk=" crossorigin="anonymous" />
+
+		
+	<body>
+		<link rel="stylesheet" href="assets/css/detail.css" />
+		<?php
+            echo "<title>ASE 230 -  Cameron Springer</title>";
+        ?>
+		<div class="container text-center mb-5">
+			<?php
+                echo "<h1>This is ASE 230 - Cameron Springer</h1>";
+            ?>
+		</div>
+		<div class="container">
+    <div class="row">
+      <div class="col-lg-5 col-md-6">
+        <div class="mb-2">
+          <img class="w-100" src="https://bootdey.com/img/Content/avatar/avatar7.png" alt="">
+        </div>
+        <div class="mb-2 d-flex">
+          <h4 class="font-weight-normal">
+              <?php
+                echo "<h4>Cameron Springer</h4>";
+              ?>
+          </h4>
+          <div class="social d-flex ml-auto">
+            <p class="pr-2 font-weight-normal">Follow on:</p>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-facebook-f"></i>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-twitter"></i>
+            </a>
+            <a href="#" class="text-muted mr-1">
+              <i class="fab fa-instagram"></i>
+            </a>
+            <a href="#" class="text-muted">
+              <i class="fab fa-linkedin"></i>
+            </a>
+          </div>
+        </div>
+        <div class="mb-2">
+          <ul class="list-unstyled">
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">
+                <?php
+                  echo "Dream Profession:";
+                ?>
+              </span>
+              <label class="media-body">
+                  <?php
+                    echo "Data Analyst";
+                  ?>
+              </label>
+            </li>
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">
+                <?php
+                  echo "Dream Company:";
+                ?>
+              </span>
+              <label class="media-body">
+                  <?php
+                    echo "Oracle";
+                  ?>
+              </label>
+            </li>
+            <li class="media">
+              <span class="w-25 text-black font-weight-normal">
+                <?php
+                  echo "Email:";
+                ?>
+              </span>
+              <label class="media-body">
+                  <?php
+                    echo "springerc4@nku.edu";
+                  ?>
+              </label>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-lg-7 col-md-6 pl-xl-3">
+        <h5 class="font-weight-normal">
+          <?php
+            echo "<h5>Short Intro</h5>";
+          ?>
+        </h5>
+        <?php
+            echo "<p>Hello, my name is Cameron Springer. I am currently a sophomore at Northern Kentucky University and majoring in Data Science. I have some prior experience in programming with Python, Java, and basic web design with HTML/CSS. For recreation, I enjoy reading, watching movies, and playing disc golf. </p>";
+        ?>
+        <div class="my-2 bg-light p-2">
+          <?php
+            echo '<p><i>"The greatest enemy of knowledge is not ignorance. It is the illusion of knowledge."</i></p>';
+            echo "<p>- Stephen Hawking</p>";
+          ?>
+        </div>
+        <div class="mb-2 mt-2 pt-1">
+          <h5 class="font-weight-normal">
+            <?php
+              echo "Top Skills";
+            ?>
+          </h5>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:55%" aria-valuenow="85" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">
+                  <?php
+                    echo "Mathematics";
+                  ?>
+              </div>
+              <span class="progress-bar-number">
+                  <?php
+                    echo "55%";
+                  ?>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:45%" aria-valuenow="70" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">
+                  <?php
+                    echo "Web Design";
+                  ?>
+              </div>
+              <span class="progress-bar-number">
+                  <?php
+                    echo "45%";
+                  ?>
+              </span>
+            </div>
+          </div>
+        </div>
+        <div class="py-1">
+          <div class="progress">
+            <div class="progress-bar" role="progressbar" style="width:30%" aria-valuenow="77" aria-valuemin="0" aria-valuemax="100">
+              <div class="progress-bar-title">
+                  <?php
+                    echo "Public Speaking";
+                  ?>
+              </div>
+              <span class="progress-bar-number">
+                  <?php
+                    echo "30%";
+                  ?>
+              </span>
+            </div>
+          </div>
+        </div>
+        <h5 class="font-weight-normal">
+            <?php
+                echo "Fun Fact";
+            ?>
+        </h5>
+        <?php
+            echo "<p>I've never ridden a bike. Sometimes I refuse to learn just so I can have something unique to use for icebreakers and I also just haven't had any interest in learning. While it's still a small minority, some might be schocked by the 6% of American adults who don't know how to ride a bike.</p>";
+        ?>
+      </div>
+    </div>
+    <hr>
+    <a href="index.php">
+      <?php
+        echo "<h4>Back to Index</h4>";
+      ?>
+    </a>
+  </div>
+	<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</html>
